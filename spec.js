de235c80fca8cefac4c53e8045f9c9c7895aec40
Kevin Qualters <kqualters@endgame.com>
2015-03-20 13:59:59 -0400

    round 2 update

--- spec.js
+++ spec.js
@@ -2,15 +2,15 @@
 browser.ignoreSynchronization = true;
 describe('march madness champs', function () {
     it('should win', function(){
-    browser.get('http://dcinno.streetwise.co/all-series/dctechmadness-bracket-is-revealed-vote-now/').then(function () {
-        element(by.id('center-flyin')).click().then(function () {
-            element(by.cssContainingText('.pds-answer-span', '3 - Endgame')).click().then(function () {
+    browser.get('http://dcinno.streetwise.co/2015/03/19/dc-inno-dctechmadness-round-2-voting-results-statistics/').then(function () {
+        //element(by.id('center-flyin')).click().then(function () {
+            element(by.cssContainingText('.pds-answer-span', 'Endgame (3rd seed)')).click().then(function () {
 
-                element(by.id('pd-vote-button8715144')).click().then(function(){
+                element(by.id('pd-vote-button8738058')).click().then(function(){
                    browser.sleep(5000);
                 });
             });
-        });
+        //});
     });
     });
 });
