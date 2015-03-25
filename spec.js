c305e79eae2143cae3796d2e52cdebf9f2c98774
Kevin Qualters <kevinq3789@gmail.com>
2015-03-25 11:16:48 -0400

    Update spec.js

--- spec.js
+++ spec.js
@@ -2,10 +2,11 @@
 browser.ignoreSynchronization = true;
 describe('march madness champs', function () {
     it('should win', function(){
+    //tweak the url    
     browser.get('http://dcinno.streetwise.co/2015/03/19/dc-inno-dctechmadness-round-2-voting-results-statistics/').then(function () {
-        //element(by.id('center-flyin')).click().then(function () {
+            //tweak this selector
             element(by.cssContainingText('.pds-answer-span', 'Endgame (3rd seed)')).click().then(function () {
-
+                //tweak this selector
                 element(by.id('pd-vote-button8738058')).click().then(function(){
                    browser.sleep(5000);
                 });
